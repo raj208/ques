e457a06586a5a83a8c86401721f4d35522676b02
RAJENDRA <rajendrafcb3087@gmail.com>
2023-02-10 13:53:22 +0530

    concept modified

--- concept.cpp
+++ concept.cpp
@@ -30,3 +30,18 @@ int main(){
 //     return 0;
 // }
  
+
+//  ****overloading function ************
+// int sum(int a, int b){
+//     return (a+b);
+// }
+
+// int sum (int a, int b, int c){
+//     return (a+b+c);
+// }
+
+// int main(){
+//     cout<<"value of 2 arguments is "<<sum(3,6)<<endl;  //9
+//     cout<<"value of 3 argumentsis "<<sum(3,6,1)<<endl; //10
+//     return 0;
+// }
