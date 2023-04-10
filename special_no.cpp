914ffd6d4af8ac8bade57cb031968a552705f5ed
RAJENDRA <rajendrajfc1021@gmail.com>
2023-04-10 22:49:22 +0530

    factorial number

--- special_no.cpp
+++ special_no.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
 using namespace std;
 
+// int fac(num){
+//     long fact
+// }
+
 int main(){
-    
+    int num;
+    int factorial = 1.0;
+    cout<<"Enter a positive number"<<endl;
+    cin>>num;
+    for (int i = 1; i <= num; i++)
+    {
+        factorial *= i;
+    }
+    cout<<factorial;
     return 0;
 }
