37b0d0ef722b52fe173696b6a54c38436d522295
RAJENDRA <rajendrajfc1021@gmail.com>
2023-04-11 23:21:18 +0530

    special number

--- special_no.cpp
+++ special_no.cpp
@@ -1,21 +1,32 @@
 #include<iostream>
 using namespace std;
 
-int num;
-int fac(num)
-{
+
+int fac (int x){
     long factorial = 1.0;
-    for (int i = 1; i <= num; i++)
+    for (int i = 1; i <= x; i++)
     {
         factorial *= i;
     }
-    return factorial;
+    return(factorial);
+    
 }
-
-int main()
-{
-    cout<<"Enter a positive number"<<endl;
+int main(){
+    int num,FacSum;
+    cout<<"Enter a number"<<endl;
     cin>>num;
-    cout<<fac<<endl;
+    int sum = 0;
+    int FacSum2 = 0;
+     while (num!=0)
+     {
+        int digit = num%10;
+        sum += digit ; //sum = sum + digit
+        FacSum = fac(digit);
+        FacSum2 += FacSum;
+        num /= 10;
+
+     }
+     cout<<"sum of num "<<sum<<endl;
+     cout<<"special number "<<FacSum2<<endl;
     return 0;
 }
