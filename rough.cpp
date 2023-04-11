4cc6cb5032abb8db988e0ea2a3b838b19f617a4e
RAJENDRA <rajendrajfc1021@gmail.com>
2023-04-11 23:22:31 +0530

    special number rough

--- rough.cpp
+++ rough.cpp
@@ -1,29 +1,20 @@
-
-//Cpp Program to check whether a given number is a Neon number or not
-#include <iostream>
+#include<iostream>
 using namespace std;
- 
-int main()
-{
-  int num;
- 
-  cout << "Enter the number: ";
-  cin >> num;
- 
-  int square = num * num;
-  int sum = 0;
- 
-  while (square > 0)
-  {
-    int lastDigit = square % 10;
-    sum = sum + lastDigit;
-    square = square / 10;
-  }
- 
-  if (sum == num)
-    cout << num << " is a Neon number";
-  else
-    cout << num << " is NOT a Neon number";
- 
-  return 0;
+
+int num;
+
+int fac(int x){
+    long factorial = 1.0;
+    for (int i = 1; i <= x; i++)
+    {
+        factorial *= i;
+    }
+    
+    return(factorial);
+}
+int main(){
+    cout<<"Enter a positive number"<<endl;
+    cin>>num;
+    cout<<fac(num);
+    return 0;
 }
