7377ff4f36ae3b31b907ac085161bf1728500918
RAJENDRA <rajendrajfc1021@gmail.com>
2023-04-12 15:57:40 +0530

    function to check frequency

--- frequency.cpp
+++ frequency.cpp
@@ -24,3 +24,43 @@ int main(){
     
     return 0;
 }
+
+// ************ function to check frequency*******************
+
+// #include<iostream>
+// using namespace std;
+
+// int r;
+// int frequency(int n){
+//     for (int i = 0; i < 10; i++)
+//     {
+//         //cout<<"The frequency of "<< i << " = ";
+//         int freq = 0;
+//         for (int j = n; j > 0 ; j /= 10)
+//         {
+//             r = j %10;
+//             if (r == i)
+//             {
+//                 freq++;
+//             }
+            
+//         }
+//         if (freq != 0)
+//         {
+//             cout<<freq<< endl;
+//         }
+        
+        
+        
+//     }
+    
+// }
+
+// int main(){
+//     int num;
+//     cout<<"Enter a number"<<endl;
+//     cin>>num;
+//     frequency(num);
+
+//     return 0;
+// }
