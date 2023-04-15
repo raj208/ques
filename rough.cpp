36b447193214f7a985c7cc8797b178033927d04b
RAJENDRA <rajendrajfc1021@gmail.com>
2023-04-15 11:46:50 +0530

    unique no compiled sucesfully

--- rough.cpp
+++ rough.cpp
@@ -1,24 +1,22 @@
+
+
 #include <iostream>
+
 using namespace std;
 
 int main() {
-    int num, digit;
-    int frequency[10] = {0}; // initialize array with 0s
-    
-    cout << "Enter a number: ";
-    cin >> num;
+  int output[] = {1, 2, 3, 4, 5}; // example output
+  int outputArray[5]; // create empty array
 
-    while (num != 0) {
-        digit = num % 10; // extract the last digit
-        frequency[digit]++; // increment the count for that digit
-        num /= 10; // remove the last digit from the number
-    }
-    
-    // print the frequency of digits
-    cout << "Frequency of digits:" << endl;
-    for (int i = 0; i < 10; i++) {
-        cout << i << " occurs " << frequency[i] << " times" << endl;
-    }
-    
-    return 0;
+  for (int i = 0; i < 5; i++) {
+    outputArray[i] = output[i]; // assign each value to the corresponding element in the array
+  }
+
+  // output the array
+  for (int i = 0; i < 5; i++) {
+    cout << outputArray[i] << " ";
+  }
+  cout << endl;
+
+  return 0;
 }
---
+++ unique_no.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n; // number of elements
+    cout << "Enter the number of digit you want to sort" << endl;
+    cin >> n;
+
+    int arr[n]; // declare array with size n
+
+    // input elements of the array
+    cout << "Enter " << n << " elements: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // sort the array in ascending order using Bubble Sort
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    // display the sorted array
+    // cout << "Sorted array: " << endl;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << arr[i];
+    // }
+
+
+    //unique_no Algorithm
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != arr[i + 1])
+        {
+            cout<<arr[i];
+        }
+        else{
+            continue;
+        }
+    }
+
+    return 0;
+}
