5c670af8ab25ac4b5aac3059620ab7e75dfe5c71
RAJENDRA <rajendrajfc1021@gmail.com>
2023-04-28 18:59:00 +0530

    optimized binary search

--- binary_search.cpp
+++ binary_search.cpp
@@ -6,14 +6,15 @@ int BinarySearch(int arr[], int size, int key)
     int start = 0;
     int end = size - 1;
     int mid = (start + end) / 2;
-
-    for (int i = 0; i < end; i++)
+    // ooptimized code 
+    // int mid = start + (end - start)/2 ;
+    while (start <= end)
     {
         if (arr[mid] == key)
         {
             return mid;
         }
-        if (arr[mid] < key)
+        if (key > arr[mid])
         {
             start = mid + 1;
         }
@@ -22,8 +23,31 @@ int BinarySearch(int arr[], int size, int key)
             end = mid - 1;
         }
         mid = (start + end) / 2;
+        // mid = start + (end - start)/2 ;
     }
     return -1;
+}
+
+int IncreasingOrder(int arr[], int size){
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    cout<<"Sorted array"<<endl;;
+    for (int i = 0; i < size; i++)
+    {
+        cout<<arr[i]<< " ";
+    }
+    cout<<endl;
+    
 }
 
 int main()
@@ -38,26 +62,27 @@ int main()
     {
         cin >> arr[i];
     }
+    IncreasingOrder(arr, size);
+    
+    // for (int i = 0; i < size - 1; i++)
+    // {
+    //     for (int j = 0; j < size - i - 1; j++)
+    //     {
+    //         if (arr[j] > arr[j + 1])
+    //         {
+    //             int temp = arr[j];
+    //             arr[j] = arr[j + 1];
+    //             arr[j + 1] = temp;
+    //         }
+    //     }
+    // }
 
-    for (int i = 0; i < size - 1; i++)
-    {
-        for (int j = 0; j < size - i - 1; j++)
-        {
-            if (arr[j] > arr[j + 1])
-            {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-
-    cout << "Sorted array ";
-    for (int i = 0; i < size; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+    // cout << "Sorted array ";
+    // for (int i = 0; i < size; i++)
+    // {
+    //     cout << arr[i] << " ";
+    // }
+    // cout << endl;
 
     cout << "Enter the key" << endl;
     cin >> key;
