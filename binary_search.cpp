1f03820c58b6e512a5f28f10a6567e938357e078
RAJENDRA <rajendrajfc1021@gmail.com>
2023-04-29 00:15:56 +0530

    optimized code

--- binary_search.cpp
+++ binary_search.cpp
@@ -5,9 +5,9 @@ int BinarySearch(int arr[], int size, int key)
 {
     int start = 0;
     int end = size - 1;
-    int mid = (start + end) / 2;
+    // int mid = (start + end) / 2;
     // ooptimized code 
-    // int mid = start + (end - start)/2 ;
+    int mid = start + (end - start)/2 ;
     while (start <= end)
     {
         if (arr[mid] == key)
@@ -22,8 +22,8 @@ int BinarySearch(int arr[], int size, int key)
         {
             end = mid - 1;
         }
-        mid = (start + end) / 2;
-        // mid = start + (end - start)/2 ;
+        // mid = (start + end) / 2;
+        mid = start + (end - start)/2 ;
     }
     return -1;
 }
