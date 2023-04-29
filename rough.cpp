ef870487527cf4007edc6c5fff16bb38cd024350
RAJENDRA <rajendrajfc1021@gmail.com>
2023-04-30 00:48:16 +0530

    binary search practice

--- rough.cpp
+++ rough.cpp
@@ -1,23 +1,67 @@
 #include<iostream>
 using namespace std;
 
+int BinarySearch(int arr[], int size, int key){
+    int start = 0;
+    int end = size -1 ;
+    int mid = start + (end-start)/2;
+    while (start <= end)
+    {
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        if (key < arr[mid] )
+        {
+            end = mid - 1;
+        }
+        else{
+            start = mid + 1;
+        }
+        mid = start + (end - start)/2;
+    }
+    return -1;
+}
 
-int main()  
-{  
-  int n, i, m=0, flag=0;  
-  cout << "Enter the Number to check Prime: ";  
-  cin >> n;  
-  m=n/2;  
-  for(i = 2; i <= m; i++)  
-  {  
-      if(n % i == 0)  
-      {  
-          cout<<"Number is not Prime."<<endl;  
-          flag=1;  
-          break;  
-      }  
-  }  
-  if (flag==0)  
-      cout << "Number is Prime."<<endl;  
-  return 0;  
-}  
+int SortedArray(int arr[], int size){
+    for (int i = 0; i < size-1 ; i++)
+    {
+        for (int j = 0; j < size - i -1 ; j++)
+        {
+            if (arr[j]>arr[j+1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }  
+    }
+    cout<<"Sorted array is ";
+    for (int i = 0; i < size; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    int size,key;
+    cout<<"Enter the size of array"<<endl;
+    cin>>size;
+
+    int arr[size];
+    cout<<"Enter the value of array"<<endl;
+    for (int i = 0; i < size; i++)
+    {
+        cin>>arr[i];
+    }
+
+    SortedArray(arr, size);
+
+    cout<<"Enter the key"<<endl;
+    cin>>key;
+
+    int index = BinarySearch(arr,size, key);
+    cout<<"The key is on Index number "<<index;
+    return 0;
+}
