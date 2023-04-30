0b023174cc32e2fed385b070dbffa16008c50ed8
RAJENDRA <rajendrajfc1021@gmail.com>
2023-04-30 12:22:28 +0530

    binary search ques 1 practice

--- rough.cpp
+++ rough.cpp
@@ -1,47 +1,79 @@
 #include<iostream>
 using namespace std;
 
-int BinarySearch(int arr[], int size, int key){
-    int start = 0;
-    int end = size -1 ;
-    int mid = start + (end-start)/2;
+int FirstOccurence(int arr[], int size, int key){
+    int start = 0,ans = 0;
+    int end = size -1;
+    int mid = start + (end - start)/2; 
     while (start <= end)
     {
         if (arr[mid] == key)
         {
-            return mid;
+            ans = mid;
+            end = mid - 1;
         }
-        if (key < arr[mid] )
+        else if (key > arr[mid])
+        {
+            start = mid + 1;
+        }
+        else if (key < arr[mid])
         {
             end = mid - 1;
         }
-        else{
+        mid = start + (end - start)/2 ;  
+    }
+    return ans;
+    
+}
+
+int LastOccurence(int arr[], int size, int key){
+    int start = 0, ans = 0;
+    int end = size - 1;
+    int mid = start + (end - start)/2;
+    while (start <= end)
+    {
+        if (arr[mid] == key)
+        {
+            ans = mid;
             start = mid + 1;
+        }
+        else if (key > arr[mid])
+        {
+            start = mid + 1;
+        }
+        else if (key < arr[mid])
+        {
+            end = mid - 1;
         }
         mid = start + (end - start)/2;
     }
-    return -1;
+    return ans;
+    
 }
 
 int SortedArray(int arr[], int size){
-    for (int i = 0; i < size-1 ; i++)
+    for (int i = 0; i < size-1; i++)
     {
-        for (int j = 0; j < size - i -1 ; j++)
+        for (int j = 0; j < size - i - 1; j++)
         {
-            if (arr[j]>arr[j+1])
+            if (arr[j] > arr[j+1])
             {
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
             }
-        }  
+            
+        }
+        
     }
-    cout<<"Sorted array is ";
+    cout<<"Sorted array"<<endl;
     for (int i = 0; i < size; i++)
     {
         cout<<arr[i]<<" ";
     }
     cout<<endl;
+    
+    
 }
 
 int main(){
@@ -50,18 +82,21 @@ int main(){
     cin>>size;
 
     int arr[size];
-    cout<<"Enter the value of array"<<endl;
+    cout<<"Enter the vlue of array"<<endl;
     for (int i = 0; i < size; i++)
     {
         cin>>arr[i];
     }
-
-    SortedArray(arr, size);
+    
+    SortedArray(arr,size);
 
     cout<<"Enter the key"<<endl;
     cin>>key;
+    
+    int Left_Most_Index = FirstOccurence(arr,size,key);
+    int Right_Most_Index = LastOccurence(arr,size,key);
 
-    int index = BinarySearch(arr,size, key);
-    cout<<"The key is on Index number "<<index;
+    cout<<"Left most index of the key is "<<Left_Most_Index<<endl;
+    cout<<"Right most index of the key is "<<Right_Most_Index<<endl;
     return 0;
 }
