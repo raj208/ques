b8f59c6b872f643c314658e6d16b05b0f3bc4935
RAJENDRA <rajendrajfc1021@gmail.com>
2023-05-03 23:15:12 +0530

    pattern

---
+++ pattern19.cpp
@@ -0,0 +1,25 @@
+// ***************important*****************
+#include<iostream>
+using namespace std;
+
+int main(){
+    int rows,column;
+    cout<<"Enter the no. of rows"<<endl;
+    cin>>rows;
+    
+    cout<<"Enter the no. of rows"<<endl;
+    cin>>column;
+    for (int i = 1; i <= column; i++)
+    {
+        for (int j = 1; j <= column - i; j++)
+        {
+            cout<<" ";
+        }
+        for (int k = 1; k <= i; k++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
