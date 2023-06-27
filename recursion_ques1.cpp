0816c97ada71ece1a66f1bcc7a03dacbd9161e42
RAJENDRA <rajendrajfc1021@gmail.com>
2023-06-27 12:31:27 +0530

    recursion

--- recursion_ques1.cpp
+++ recursion_ques1.cpp
@@ -1,3 +1,6 @@
+// where recursive relation is at end, it's called tail recursion
+// where recursive relation is at not end, it's called head recursion
+
 #include<iostream>
 using namespace std;
 
@@ -13,12 +16,42 @@ int fac(int num)
     }
 }
 
+int power(int num)
+{
+    if (num == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 2*power(num  -1);
+    }
+    
+}
+
+
+
+int print(int num)
+{
+    if (num==0)    //base condition
+    {
+        return{};
+    }
+    cout<<num<<" ";  //processing
+
+    print(num-1);  //recursive recursion
+}
+
 int main(){
     int num;
     cout<<"Enter a number"<<endl;
     cin>>num;
 
-    int ans = fac(num);
-    cout<<ans<<endl;
+    // int ans = fac(num);
+    // cout<<ans<<endl;
+
+    // int ans = power( num);
+    // cout<<ans<<endl;
+    print(num);
     return 0;
 }
